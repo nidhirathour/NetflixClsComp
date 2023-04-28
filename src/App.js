ba78a7ede0dd9838374f647f9b2e07eff330f628
Nidhi Rathour <96580904+nidhirathour@users.noreply.github.com>
2023-04-28 11:04:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Netflix from './class-Component/Class';
+
+function App() {
+  return (
+    <div className="App">
+      <Netflix/>
+    </div>
+  );
+}
+
+export default App;
